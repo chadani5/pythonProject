54f0b34c60ccde06bea53abdd4f9af15070ba0c1
chadani5 <np03cs4s230170@heraldcollege.edu.np>
2023-03-23 21:40:24 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+def avg(num1,num2,num3):
+    return(num1+num2+num3)/3.0
+num1=10
+num2=20
+num3=30
+average=(num1,num1,num3)
+print("the average of",num1,",",num2,",","and",num3,"is",average)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
